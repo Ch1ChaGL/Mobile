e457d08ea4cc0b891c7bf71ae933f77c8b678695
Danil Markov <markov.danil.04@mail.ru>
2023-11-19 22:16:18 +0300

    16

--- 16/app/src/main/java/com/example/a16/Main.java
+++ 16/app/src/main/java/com/example/a16/Main.java
@@ -2,11 +2,17 @@ package com.example.a16;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.time.Duration;
 
 public class Main extends AppCompatActivity {
     @Override
@@ -20,8 +26,34 @@ public class Main extends AppCompatActivity {
     }
 
     public void clickBtn2(View view){
-        Toast toast = Toast.makeText(getApplicationContext(), "Пример для кнопки 2", Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.TOP, 0,0);
+        Toast toast = Toast.makeText(getApplicationContext(), "Пример для кнопки 2 (по центру)", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER, 0,0);
         toast.show();
+    }
+    public void clickBtn3(View view){
+        Toast toast = Toast.makeText(getApplicationContext(), "Пример с картинкой", Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER, 0,0);
+        LinearLayout toastContainer = (LinearLayout) toast.getView();
+        ImageView img = new ImageView(getApplicationContext());
+        img.setImageResource(R.drawable.notification);
+        toastContainer.addView(img, 0);
+        toast.show();
+    }
+
+    public void clickBtn4(View view){
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast_four_btn, (ViewGroup) findViewById(R.id.custom_toast_container));
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER, 0,0);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+
+    public void clickBtn5(View view){
+
+    }
+    public void clickBtn6(View view){
+        
     }
 }
